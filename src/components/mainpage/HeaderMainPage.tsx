3e0fae4872bc7154dc395591b1d5a38be71ec70f
Damaroo <levkichigin@yandex.ru>
2024-12-27 01:44:42 +0300

    home page start

--- src/components/mainpage/HeaderMainPage.tsx
+++ src/components/mainpage/HeaderMainPage.tsx
@@ -1,12 +1,21 @@
 import { useTranslation } from 'react-i18next'
 import './mainpage.css'
+import avatar from '/Projects/React/my-resume/src/assets/images/myAvatar.jpg'
+
 export default function HeaderMainPage() {
   const { t } = useTranslation()
 
   return (
     <>
-      <header className="mainPage">
-        <h1>{t('header.h1')}</h1>
+      <header className="headerHomePage">
+        <h1>{t('home.h1')}</h1>
+        <p>{t('home.about')}</p>
+        <img
+          sizes="100"
+          className="rounded-2xl max-w-[50%]"
+          src={avatar}
+          alt="My photo, beatiful man with long hair"
+        />
       </header>
     </>
   )
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import HeaderMainPage from '../components/mainpage/HeaderMainPage'
 export default function MainPage() {
   return (
     <main className=" h-[100vh] box-border ">
-      <section className="container">
+      <section className="m-auto py-20 px-40 max-w-[80%]">
         <HeaderMainPage />
       </section>
     </main>

4a942d4aa0e0cdef9d59d679828d80dc511d8956
Damaroo <levkichigin@yandex.ru>
2024-12-28 19:54:04 +0300

    home page fixes typing h1

--- src/components/aside/Navigation.tsx
+++ src/components/aside/Navigation.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 export default function Navigation() {
   const { t } = useTranslation()
   const styleNavLink =
-    'py-4 px-5    dark:hover:bg-[var(--hover-bg-dark-color)] hover:bg-[var(--hover-bg-color)]   rounded-lg rounded-l-none border-l-4  tracking-wide font-bold'
+    'text-[1.5rem] py-4 px-5   dark:hover:bg-[var(--hover-bg-dark-color)] hover:bg-[var(--hover-bg-color)]   rounded-lg rounded-l-none border-l-4  tracking-wide font-bold'
 
   const styleActiveNavLink =
     'border-[var(--link-color)] text-[var(--link-color)]  dark:border-[var(--dark-link-color)] dark:text-[var(--dark-link-color)]'
@@ -12,7 +12,7 @@ export default function Navigation() {
     'border-[var(--neutral-color)] dark:border-[var(--hover-bg-dark-color)] text-[var(--main-color)] dark:text-[var(--main-dark-color)]'
   return (
     <nav className="w-[100%] ">
-      <ul className="flex flex-col text-2xl  gap-[1.6rem]  justify-center items-start pl-4 py-2   ">
+      <ul className="flex  flex-col text-2xl  gap-[1.6rem]  justify-center items-start pl-4 py-2   ">
         <li>
           <NavLink
             id="home"

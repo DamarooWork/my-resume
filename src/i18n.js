0565c70d4308cfefeed94af15ae015e1e8a645b1
Damaroo <levkichigin@yandex.ru>
2025-01-03 02:56:09 +0300

    404 page

--- src/i18n.js
+++ src/i18n.js
@@ -15,8 +15,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
-    fallbackLng: 'en',
+    debug: false,
     interpolation: {
       escapeValue: false,
     },

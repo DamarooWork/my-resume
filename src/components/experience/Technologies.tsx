9209ab3f2deb0bea64d8c6b0977648cfcd497d28
Damaroo <levkichigin@yandex.ru>
2025-01-07 00:28:48 +0300

    exp

--- src/components/experience/Technologies.tsx
+++ src/components/experience/Technologies.tsx
@@ -1,11 +1,25 @@
 import Technology from './Technology'
-import reactImg from '/Projects/React/my-resume/src/assets/icons/experiencePage/react.png'
+import react from '/Projects/React/my-resume/src/assets/icons/experiencePage/react.png'
 import typescript from '/Projects/React/my-resume/src/assets/icons/experiencePage/typescript.png'
+import reduxDark from '/Projects/React/my-resume/src/assets/icons/experiencePage/reduxDark.png'
+import vite from '/Projects/React/my-resume/src/assets/icons/experiencePage/vite.png'
+import tailwind from '/Projects/React/my-resume/src/assets/icons/experiencePage/tailwind.png'
+import nodejs from '/Projects/React/my-resume/src/assets/icons/experiencePage/nodejs.png'
+import js from '/Projects/React/my-resume/src/assets/icons/experiencePage/js.png'
+import html from '/Projects/React/my-resume/src/assets/icons/experiencePage/html.png'
+import css from '/Projects/React/my-resume/src/assets/icons/experiencePage/css.png'
 export default function Technologies() {
   return (
-    <div className="grid grid-cols-[repeat(5,_1fr)]  gap-20 ">
-      <Technology title="TypeScript" img={{ src: typescript, alt: 'ts' }} />
-      <Technology title="React" img={{ src: reactImg, alt: 'react' }} />
+    <div className="technologies grid grid-cols-1 md:grid-cols-[repeat(2,_1fr)] xl:grid-cols-[repeat(3,_1fr)] 2xl:grid-cols-[repeat(4,_1fr)]  gap-20 ">
+      <Technology title="React" img={{ src: react, alt: 'react' }} />
+      <Technology title="typescript" img={{ src: typescript, alt: 'ts' }} />
+      <Technology title="Redux" img={{ src: reduxDark, alt: 'Redux' }} />
+      <Technology title="Vite" img={{ src: vite, alt: 'vite' }} />
+      <Technology title="tailwind" img={{ src: tailwind, alt: 'tailwind' }} />
+      <Technology title="NodeJS" img={{ src: nodejs, alt: 'nodejs' }} />
+      <Technology title="javascript" img={{ src: js, alt: 'js' }} />
+      <Technology title="HTML" img={{ src: html, alt: 'html' }} />
+      <Technology title="CSS" img={{ src: css, alt: 'css' }} />
     </div>
   )
 }
--- src/components/experience/Technology.tsx
+++ src/components/experience/Technology.tsx
@@ -1,13 +1,13 @@
 export default function Technology({ img, title }: ITechnology) {
   return (
     <section className=" m-auto">
-      <div className="technology relative   cursor-pointer p-4 rounded-full w-52 h-52  flex shadow-xl hover:shadow-2xl ">
+      <div className="technology relative   cursor-pointer p-4 rounded-full w-52 h-52  flex shadow-xl hover:shadow-2xl bg-[var(--section-color)] dark:bg-[var(--dark-section-color)]">
         <img
-          className="absolute top-[10%] left-[10%]  rounded-full w-[80%] h-auto m-auto  animate-[spin_4s_linear_infinite] "
+          className="absolute top-[20%] left-[20%]   w-[60%] h-auto m-auto transition-all ease-linear duration-500  will-change-transform"
           src={img.src}
           alt={img.alt}
         />
-        <h3 className=" absolute w-[80%] h-[100%] text-center  m-auto pt-[30%]  z-10 opacity-0 text-3xl font-bold">
+        <h3 className="text-[0] absolute left-[10%] w-[80%] h-[100%] text-center  pt-[30%]  z-10 opacity-0  font-bold will-change-contents">
           {title}
         </h3>
       </div>
